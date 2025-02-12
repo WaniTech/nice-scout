7fc1a69905dd2dd0bb45a8b9e1a201a98e3680d6
WaniTech <andersah24@hotmail.com>
2025-02-12 19:07:41 +0100

    initial commit

---
+++ app/(tabs)/Message.tsx
@@ -0,0 +1,46 @@
+import { Stack, useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
+
+export default function MyJobsPage() {
+  const router = useRouter();
+  const [notificationsEnabled, setNotificationsEnabled] = useState(true);
+  const [darkModeEnabled, setDarkModeEnabled] = useState(false);
+  const [locationAccessEnabled, setLocationAccessEnabled] = useState(true);
+
+  return (
+    <>
+      <Stack.Screen options={{ headerShown: false }} />
+      <View style={styles.container}>
+        <Text style={styles.header}>Message!</Text>
+        <ScrollView>
+        </ScrollView>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    padding: 20,
+  },
+  header: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  settingItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: '#E0E0E0',
+  },
+  settingText: {
+    fontSize: 18,
+  },
+});
